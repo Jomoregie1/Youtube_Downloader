205d16b7225362399b5e2a035687ab787549a1b7
jo181 <jo181@student.le.ac.uk>
2022-09-17 11:14:29 +0100

    Added functionality to download youtube video

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ youtube = YouTube(link)
 views = youtube.views
 title = youtube.title
 length = youtube.length
+file = "/Users/joseph/Documents/Youtube_videos"
 
 print(f"The title of this video {title}")
 print(f"The total number of is: {views}")
 print(f"The length of this video is: {length}")
+
+ys = youtube.streams.get_highest_resolution()
+ys.download(file)
+print(f"Your Video has been downloaded and can be found{file} ")
 
 
 
