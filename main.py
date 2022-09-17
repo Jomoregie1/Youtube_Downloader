ffc4670849f56a26df93369c8d3bca2bc8fc0a28
jo181 <jo181@student.le.ac.uk>
2022-09-17 11:02:10 +0100

    Added video description to the python code.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from pytube import Youtube
+from pytube import YouTube
+
+link = input("Enter the Url here: ")
+youtube = YouTube(link)
+views = youtube.views
+title = youtube.title
+length = youtube.length
+
+print(f"The title of this video {title}")
+print(f"The total number of is: {views}")
+print(f"The length of this video is: {length}")
+
+
+
+
+
